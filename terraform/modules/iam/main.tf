7930832727f4a7e03edefac243baef1762d417b9
Srinu T <tsrinu00@gmail.com>
2026-03-07 22:27:24 +0530

    Add IAM Terraform module

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,80 @@
+# IAM Module for StreamPlatform
+
+# Cluster IAM Role
+resource "aws_iam_role" "cluster_role" {
+  name = "${var.cluster_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Node Group IAM Role
+resource "aws_iam_role" "node_role" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_role.name
+}
+
+# Read-only Policy for Developers
+resource "aws_iam_policy" "developer_readonly" {
+  name        = "StreamPlatformDeveloperReadOnly"
+  description = "Read-only access for developers"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "eks:Describe*",
+        "eks:List*",
+        "ec2:Describe*",
+        "s3:List*",
+        "s3:Get*",
+        "rds:Describe*",
+        "elasticache:Describe*"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}

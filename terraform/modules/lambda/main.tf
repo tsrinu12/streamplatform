b4ebceef2970abdc3294fe9d800f71be4fe89f12
Srinu T <tsrinu00@gmail.com>
2026-03-07 22:02:58 +0530

    Add Lambda Terraform module

---
+++ terraform/modules/lambda/main.tf
@@ -0,0 +1,60 @@
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  timeout       = var.timeout
+  memory_size   = var.memory_size
+
+  filename         = var.filename
+  source_code_hash = filebase64sha256(var.filename)
+
+  environment {
+    variables = var.environment_variables
+  }
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = var.function_name
+    }
+  )
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = var.log_retention_days
+
+  tags = var.common_tags
+}

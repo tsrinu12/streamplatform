ccb2901314a5cb12d278e55247dd93edf7eea48b
Srinu T <tsrinu00@gmail.com>
2026-03-07 21:38:39 +0530

    Add AWS MSK cluster and configuration resources

---
+++ terraform/modules/msk/main.tf
@@ -0,0 +1,72 @@
+resource "aws_msk_cluster" "main" {
+  cluster_name           = var.cluster_name
+  kafka_version          = var.kafka_version
+  number_of_broker_nodes = var.number_of_broker_nodes
+
+  broker_node_group_info {
+    instance_type   = var.instance_type
+    client_subnets  = var.subnet_ids
+    security_groups = var.security_group_ids
+
+    storage_info {
+      ebs_storage_info {
+        volume_size = var.ebs_volume_size
+      }
+    }
+  }
+
+  encryption_info {
+    encryption_in_transit {
+      client_broker = "TLS"
+      in_cluster    = true
+    }
+    encryption_at_rest_kms_key_arn = var.kms_key_arn
+  }
+
+  configuration_info {
+    arn      = aws_msk_configuration.main.arn
+    revision = aws_msk_configuration.main.latest_revision
+  }
+
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = aws_cloudwatch_log_group.msk.name
+      }
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.cluster_name
+    }
+  )
+}
+
+resource "aws_msk_configuration" "main" {
+  name              = "${var.cluster_name}-config"
+  kafka_versions    = [var.kafka_version]
+  server_properties = <<PROPERTIES
+auto.create.topics.enable=true
+default.replication.factor=3
+min.insync.replicas=2
+num.io.threads=8
+num.network.threads=5
+num.partitions=3
+num.replica.fetchers=2
+replica.lag.time.max.ms=30000
+socket.receive.buffer.bytes=102400
+socket.request.max.bytes=104857600
+socket.send.buffer.bytes=102400
+unclean.leader.election.enable=true
+zookeeper.session.timeout.ms=18000
+PROPERTIES
+}
+
+resource "aws_cloudwatch_log_group" "msk" {
+  name              = "/aws/msk/${var.cluster_name}"
+  retention_in_days = var.log_retention_days
+  tags              = var.tags
+}
